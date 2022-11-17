0a5c85b6d4d9d5b995051a27afc96af0c7c9c82c
Yaroslav <garbar.jarek@gmail.com>
2022-11-17 19:04:27 +0100

    change population size and gen count in ga algorythm

--- Moduł 1/m/Projekt/optimization.m
+++ Moduł 1/m/Projekt/optimization.m
@@ -8,13 +8,13 @@ if regulator == 1
     lb = [1,1,0.01];
     %Ograniczenia górne parametrów
     ub = [192,192,Inf];
-    opts = optimoptions('ga', 'MaxStallGenerations', 80, 'PopulationSize',250); 
+    opts = optimoptions('ga', 'MaxStallGenerations', 100, 'PopulationSize',200); 
     [X,fval,exitflag] = ga(@DMC_optimization,3,[-1 1 0],[0],[],[],lb,ub,[],[1 2],opts);
     fprintf('DMC: \nN=%f; Nu=%f; lambda=%f;\n', X)
 else
     lb = [0, 15, 0];
     ub = [5, 100, 0.5];
-    opts = optimoptions('ga', 'MaxStallGenerations', 80, 'PopulationSize',250); 
+    opts = optimoptions('ga', 'MaxStallGenerations', 100, 'PopulationSize',200); 
     [X,fval,exitflag] = ga(@PID_optimization,3,[],[],[],[],lb,ub,[],[1 2],opts);
     fprintf('PID: \nK=%f; Ti=%f; Td=%f; Ts=0.5;\n', X)
 end
