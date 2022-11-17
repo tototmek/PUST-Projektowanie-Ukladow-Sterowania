69e9b8c1c486dcaa35c5c06db120f68cfaf7b738
Adam Dąbkowski <adamdobrus@gmail.com>
2022-11-17 23:32:26 +0100

    małe zmiany w plikach na potrzeby sprawozdania

--- Moduł 1/m/Projekt/zadanie_3.m
+++ Moduł 1/m/Projekt/zadanie_3.m
@@ -8,22 +8,7 @@ Ypp=2.2;
 
 U_step=1.75;
 
-U(1:iterations)=Upp;
-U(12:iterations)=U_step;
-
-Y(1:11)=Ypp;
-Y(12:iterations)=0;
-
-for k=12:iterations
-    Y(k)=symulacja_obiektu5Y_p1(U(k-10),U(k-11),Y(k-1),Y(k-2));
-end
-
-
-%Wyznaczenie zestawu liczb odpowiedzi skokowej dla DMC
-
-for k=13:iterations
-    s(k-12)=(Y(k)-Ypp)/0.5;
-end
+s = DMC_s(iterations, U_step);
 
 stairs(s, 'LineWidth', 1.1);
 xlabel('$k$', 'Interpreter','latex');
