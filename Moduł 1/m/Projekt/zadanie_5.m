d4ce2b88d3a7a4b1a783c8ec57e067d72ec81d6b
Adam Dąbkowski <adamdobrus@gmail.com>
2022-11-20 12:00:15 +0100

    modyfikacje wykresów

--- Moduł 1/m/Projekt/zadanie_5.m
+++ Moduł 1/m/Projekt/zadanie_5.m
@@ -1,21 +1,27 @@
 clear all;
+set(groot,'defaultAxesTickLabelInterpreter','latex');  
 init;
 
 % Wybór regulatora (1-DMC, 2-PID)
-regulator = 2;
+regulator = 1;
 
 % Parametry DMC
-D = 195;
+% D = 195;
+% N = 21;
+% Nu = 7;
+% lambda = 0.1;
+
+D = 216;
 N = 21;
 Nu = 7;
-lambda = 0.1;
+lambda = 0.01;
 
 % Nastawy PID i wyliczenie parametrów PID
-K=5; Ti=7; Td=4;
+% K=5; Ti=7; Td=4;
 
-% K = 6.019519547;
-% Ti = 5.903171;
-% Td = 4.541852;
+K = 6.019519547;
+Ti = 5.903171;
+Td = 4.541852;
 
 
 if regulator == 1
@@ -27,23 +33,29 @@ end
 disp(E);
 
 subplot(2,1,1);
-stairs(U, 'k', 'LineWidth', 1.5);
-xlabel('$k$', 'Interpreter','latex');
-ylabel('$u$', 'Interpreter','latex');
-legend('$u$', 'Interpreter','latex');
-
-% set(groot,'defaultAxesTickLabelInterpreter','latex');  
-% set(gcf,'units','points','position',[100 100 450 300]);
-% print('plots/zadanie_5/zad_5_u','-depsc','-r400');  % Zapisywanie wykresu
-
-subplot(2,1,2);
-stairs(Y, 'LineWidth', 1.5);
+stairs(Y, 'LineWidth', 1.1);
 hold on; 
-stairs(Y_zad,':', 'LineWidth', 1.5);
+stairs(Y_zad,':', 'LineWidth', 1.1);
 xlabel('$k$', 'Interpreter','latex');
 ylabel('$y$', 'Interpreter','latex');
 legend('$y$', '$y^{\mathrm{zad}}$', 'Interpreter','latex');
+title(strrep(sprintf("$E=%f$", E),'.',','), 'Interpreter','latex')
+yl = get(gca,'YTickLabel');
+set(gca, 'YTickLabel', strrep(yl(:),'.',','))
+xl = get(gca,'XTickLabel');
+set(gca, 'XTickLabel', strrep(xl(:),'.',','))
+hold on; 
 
-set(groot,'defaultAxesTickLabelInterpreter','latex');  
+subplot(2,1,2);
+stairs(U, 'k', 'LineWidth', 1.1);
+xlabel('$k$', 'Interpreter','latex');
+ylabel('$u$', 'Interpreter','latex');
+legend('$u$', 'Interpreter','latex');
+yl = get(gca,'YTickLabel');
+set(gca, 'YTickLabel', strrep(yl(:),'.',','))
+xl = get(gca,'XTickLabel');
+set(gca, 'XTickLabel', strrep(xl(:),'.',','))
+
+
 set(gcf,'units','points','position',[100 100 450 300]);
-print('plots/zadanie_5/zad_5_pid','-depsc','-r400');  % Zapisywanie wykresu
+print('plots/zadanie_5/zad_6_dmc','-depsc','-r400');  % Zapisywanie wykresu
