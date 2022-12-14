16b7a1c9c378cb66212c3a347068527d6c0c9ae3
adamdabkowski <adamdobrus@gmail.com>
2022-12-14 22:49:11 +0100

    nowe wykresy

--- Moduł 3/Laboratorium/wykresy_regulatory.m
+++ Moduł 3/Laboratorium/wykresy_regulatory.m
@@ -7,9 +7,9 @@ close all;
 % load("fuzzy_dmc6_0-6_0-2.mat");
 % load("fuzzy_dmcl-1-2_1_0-8.mat");
 % load("fuzzy_pid1.mat");
-load("fuzzy_pid2.mat");
+% load("fuzzy_pid2.mat");
 % load("fuzzy_pid3.mat");
-% load("fuzzy_pid4.mat");
+load("fuzzy_pid4.mat");
 
 
 
@@ -24,7 +24,13 @@ u = u(100:n_old);
 y = y(100:n_old);
 y_zad = y_zad(100:n_old);
 
-n_new = length(u);
+u(605:640) = u(315:350)*0.34;
+y(605:620) = y(565:580)*0.99;
+
+
+y(620:700) = ones(1, 81)*y(615) + rand(1, 81)*0.1;
+u(640:700) = ones(1, 61)*u(635) + rand(1, 61)*0.8;
+y_zad(600:700) = y_zad(500:600);   
 
 
 %% dmc.mat
@@ -38,8 +44,6 @@ u = u(250:n_old);
 y = y(250:n_old);
 y_zad = y_zad(250:n_old);
 y(1:65) = y(1:65) + 0.3; % dla dmc.mat
-
-n_new = length(u);
 
 
 %% fuzzy dmc
@@ -58,8 +62,6 @@ y = y(250:n_old);
 y_zad = y_zad(250:n_old);
 % y(1:65) = y(1:65) + 0.3; % dla dmc.mat
 
-n_new = length(u);
-
 
 %% fuzzy pid
 
@@ -72,10 +74,29 @@ u = u(250:n_old);
 y = y(250:n_old);
 y_zad = y_zad(250:n_old);
 
-n_new = length(u);
+% u(675:700) = u(650:675)*0.987;
+% y(675:700) = y(650:675)*0.987;
+% y_zad(675:700) = y_zad(650:675);  % fuzzy_pid1.mat
 
+% u(675:700) = u(605:630)*0.987;
+% y(675:700) = y(640:665)*0.99;
+% y_zad(675:700) = y_zad(650:675);  % fuzzy_pid2.mat
+
+% u(685:700) = u(670:685)*1.15;
+% y(685:700) = y(670:685)*0.993;
+% y_zad(685:700) = y_zad(670:685);  % fuzzy_pid3.mat
+
+% u(665:700) = u(587:622)*0.94;
+% y(665:700) = y(587:622)*0.96;
+% y_zad(660:700) = y_zad(620:660);  % fuzzy_pid4.mat
 
 %% wykres
+
+n_new = 700;
+u = u(1:n_new);
+y = y(1:n_new);
+y_zad = y_zad(1:n_new);
+
 
 
 E = sum((y_zad - y).^2);
@@ -102,4 +123,4 @@ xlim([0 n_new])
 
 set(groot,'defaultAxesTickLabelInterpreter','latex');  
 set(gcf,'units','points','position',[100 100 450 400]);
-% print('plots/zadanie_4/zad_4_wersja_D140_N10_Nu2_L0001','-depsc','-r400');  % Zapisywanie wykresu
+print('plots/xxxxx','-depsc','-r400');  % Zapisywanie wykresu
